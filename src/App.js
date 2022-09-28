5dd91a981715826f8458b8f36244bfc58e086dc5
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-09-28 07:15:19 +0000

    React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/single",
+    element: <Single />,
+  },
+  {
+    path: "/write",
+    element: <Write />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+]);
+
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router} />{" "}
     </div>
   );
 }
